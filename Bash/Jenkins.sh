cd293a4ec61ea629fa6e5b74bae1f15668e14c3d
Sergey <fool.groop@gmail.com>
2022-04-20 12:06:49 +0300

    gsg

---
+++ Bash/Jenkins.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo su
+sudo apt update
+sudo apt -y install openjdk-8-jdk
+sudo apt update -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo systemctl status docker
---
+++ EC2/Instances.tf
@@ -0,0 +1,79 @@
+provider "aws" {
+ region = "eu-central-1"
+}
+
+resource "aws_instance" "Jenkins" {
+ ami = "ami-0d527b8c289b4af7f"
+ instance_type = "t2.micro"
+ user_data = file("Bash/Jenkins.sh")
+ vpc_security_group_ids = [aws_security_group.Jenkins.id]
+ key_name = "Key/Frankfurt1.pem"
+ tags = {
+    Name = "Jenkins"
+  }
+}
+
+resource "aws_security_group" "Jenkins" {
+
+   ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Jenkins"
+  }
+}
+
+resource "aws_instance" "Prod" {
+ ami = "ami-0d527b8c289b4af7f"
+ instance_type = "t2.micro"
+ user_data = file("Bash/Docker.sh")
+ vpc_security_group_ids = [aws_security_group.Prod.id]
+ key_name = "Key/Frankfurt1.pem"
+ tags = {
+    Name = "Prod"
+  }
+}
+
+resource "aws_security_group" "Prod" {
+
+   ingress { 
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Prod"
+  }
+}
