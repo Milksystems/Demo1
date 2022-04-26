33e528714e18f24f074aa6e3e63493c00b9bd429
Sergey <fool.groop@gmail.com>
2022-04-26 12:49:20 +0300

    sdg

--- EC2/Jenkins.sh
+++ EC2/Jenkins.sh
@@ -11,10 +11,3 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
 sudo apt-get update -y
 sudo apt-get install jenkins -y
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-apt-cache policy docker-ce
-sudo apt install docker-ce -y
-sudo systemctl status docker
